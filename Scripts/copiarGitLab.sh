19268d896e56d9d8dedff2b6331e8df1089071c2
asartorio <alejandro.sartorio@gmail.com>
2018-09-01 12:29:03 -0300

    bk automático

---
+++ Scripts/copiarGitLab.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+sudo  cp  /home/ale/vbprog/WgestionDev/*.dca  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.dsr  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.dsx  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.frm  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.frx  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.jar  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.vbp  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.vbp  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.xml  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+sudo  cp  /home/ale/vbprog/WgestionDev/*.bas  /home/ale/Ale/wsf/vbprog/1/WGestionDev/
+
+
+cd /home/ale/Ale/wsf/vbprog/WGestionDev/1/
+
+
+git add .
+
+git commit -m "bk automático"
+
+git push origin master
+
+
+
+
+                                                                         
